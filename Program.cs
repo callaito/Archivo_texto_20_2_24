c589994834647007e07fa07100c0246077fadc61
callaito <radelinrsm029@gmail.com>
2024-03-07 14:20:02 -0400

    Se agrega el Proyecto

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Gestion de archivos");
+Console.WriteLine();
+
+//Creacion de archivo
+StreamWriter archivo = new StreamWriter("archivo.text");
+archivo.WriteLine("Contenido del archivo");
+archivo.WriteLine("Contenido en otra linea");
+archivo.Close();
